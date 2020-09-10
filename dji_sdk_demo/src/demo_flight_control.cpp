7b3af2678150640027bc32de2bda3a389e84decf
XisHungry <60353325+XisHungry@users.noreply.github.com>
2020-09-10 16:06:14 +0800

    Update demo_flight_control.cpp

--- dji_sdk_demo/src/demo_flight_control.cpp
+++ dji_sdk_demo/src/demo_flight_control.cpp
@@ -129,7 +129,7 @@ localOffsetFromGpsOffset(geometry_msgs::Vector3&  deltaNed,
 	
   ofstream Location;
   Location.open("Location.txt");
-  Location <<"Latitude=" fixed << setprecision(6) << target.latitude  << ",Longitude="  << fixed << setprecision(6) << target.longitude << ",Altitude=" << fixed << setprecision(2) << CurrentAlt << "\n";
+  Location <<"Latitude=" << fixed << setprecision(6) << target.latitude  << ",Longitude="  << fixed << setprecision(6) << target.longitude << ",Altitude=" << fixed << setprecision(2) << CurrentAlt << "\n";
 
   ROS_INFO("Lat=%f, Long=%f, Alt=%f", target.latitude, target.longitude, CurrentAlt);
 }
